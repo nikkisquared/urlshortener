f2511572be845d2ee609f92d00265f3e3b6f1a90
mno <mno00.email@gmail.com>
2014-07-22 19:38:11 -0600

    main page renders differently based on access method

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def hello_world():
-    return render_template("index.html")
+
+    print "my method: ", request.method
+    print request.data
+    
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        return "YOU GOT HERE FROM POST!"
 
 if __name__ == '__main__':
     app.run(debug=True)
