ec0ccb1f1a2aedaf08afc28b65bdb08eae1af892
mno <mno00.email@gmail.com>
2014-07-22 21:29:35 -0600

    now prints request form data

--- hello.py
+++ hello.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 @app.route('/', methods=["GET", "POST"])
 def hello_world():
 
     print "my method: ", request.method
-    print request.data
+    print request.form.get("originalURL")
     
     if request.method == "GET":
         return render_template("index.html")
+
     elif request.method == "POST":
-        return "YOU GOT HERE FROM POST!"
+
+        originalURL = request.form.get("originalURL")
+        originalURL = str(originalURL)
+        return u"YOUR URL: " + originalURL
+
 
 if __name__ == '__main__':
     app.run(debug=True)
